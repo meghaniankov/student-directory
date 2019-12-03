010b17327527cfa477c6d824bb2c5c240d823f41
Meghan <meghan.e.mccue@gmail.com>
2019-12-03 15:59:38 +0000

    Exercise 5

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,19 +1,27 @@
 def input_students
-    puts "Please enter the names of the students"
+    puts "Please enter the following info"
     puts "To finish, just hit return twice"
-    #creates an empty array
     students = []
-    # get the first name
+    puts "Enter name: "
     name = gets.chomp
-    # while the name is not empty, repeat this code
+    puts "Hobby: "
+    hobby = gets.chomp
+    puts "Country: "
+    country = gets.chomp
     while !name.empty? do
-        #add the student hash to the array
-        students << {name: name, cohort: :november}
+
+        students << {name: name, hobby: hobby, country: country, cohort: :november}
         puts "Now we have #{students.count} students"
-        #get another name from the user"
+
+        puts "Name: "
         name = gets.chomp
+        if !name.empty? 
+            puts "Hobby: "
+            hobby = gets.chomp
+            puts "Country: "
+            country = gets.chomp
+        end
     end
-    #return the array of students
     students
 end
 
@@ -25,7 +33,7 @@ end
 def print(students)
     student_count = 0
     until student_count == students.length do
-        puts "#{students[student_count][:name]} (#{students[student_count][:cohort]} cohort)"
+        puts "#{students[student_count][:name]} Hobby: #{students[student_count][:hobby]} Country: #{students[student_count][:country]} (#{students[student_count][:cohort]} cohort)"
         student_count += 1
     end
 end
@@ -33,7 +41,7 @@ end
 def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
-#nothing happens until we call the method
+
 students = input_students
 print_header
 print(students)
